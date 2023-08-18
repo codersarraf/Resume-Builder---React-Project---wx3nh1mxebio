49db3f1b9b049b435171b3bca62657afc48005e3
codersarraf <amitsarraf6@gmail.com>
2023-08-18 22:51:38 +0530

    commit form input changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import  TextField  from '@material-ui/core/TextField'
-import Grid from '@material-ui/core/Grid';
+
 import Button from '@material-ui/core/Button';
 import {Icon}  from '@material-ui/core';
 
@@ -23,128 +23,128 @@ function Form(props) {
                 
                 
                 <h1 id="heading">Resume-Builder</h1>
-                    <Grid container spacing={2}  >
-                        <Grid item lg={12} md={12} sm={12} xs={12} >
+                    <div container spacing={2}  >
+                        <div>
                               <h1>Basics</h1>
-                        </Grid>
-                        <Grid item lg={1} md={2} >
+                        </div>
+                        <div>
                             <img id="img" src={props.formdata.addimage}  />
-                        </Grid>
-                        <Grid item lg={10} md={12} sm={12} xs={12}  id="abc">
-                             <TextField  type="text" id="fullname" name="fullname" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='inputs' width="100%" label="Full Name" variant="outlined"    />
-                             <TextField   type="file" component="label" name="addimage" id='add-image' onChange={async(e)=>handleInput(e.target.name, URL.createObjectURL(e.target.files[0]))}><Icon color="primary">add_circle</Icon>Add-Image</TextField>
-          
-                        </Grid>
-                        <Grid item lg={12}md={12} sm={12} xs={12} >
-                             <TextField  type="email"  id="email" name="email" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' label="Email Address" variant="outlined"   />
-                        </Grid>
-                        <Grid item lg={6} md={12} sm={12} xs={12}  >
-                             <TextField  type="number" id="phoneNumber" name="phonenumber" onChange={(e)=>handleInput(e.target.name, e.target.value)} className="small-input" label="Phone Number" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12} sm={12} xs={12}  >
-                             <TextField  type="url" id="website" name="website" onChange={(e)=>handleInput(e.target.name, e.target.value)} className="small-input" label="Website" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={12} md={12} sm={12} xs={12}  >
-                             <TextField  type="text" id="headline" name="headline" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' label="Headline" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={12} md={12} sm={12} xs={12}  >
-                             <TextField  type="text" id="summary" name="summary" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' label="Summary" variant="outlined"    />
-                        </Grid>
+                        </div>
+                        <div id="abc">
+                             <input  type="text" id="fullname" name="fullname" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='inputs' width="100%" placeholder="Full Name"     />
+                             <TextField   type="file"  name="addimage" id='add-image' class="profile-img" onChange={async(e)=>handleInput(e.target.name, URL.createObjectURL(e.target.files[0]))}><Icon color="primary">add_circle</Icon>Add-Image</TextField>
+                              <label htmlFor="add-image" className='btn-label'>Add Image...</label>
+                        </div>
+                        <div>
+                             <input  type="email"  id="email" name="email" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' placeholder="Email Address" />
+                        </div>
+                        <div>
+                             <input  type="number" id="phoneNumber" name="phonenumber" onChange={(e)=>handleInput(e.target.name, e.target.value)} className="small-input" placeholder="Phone Number"     />
+                        </div>
+                        <div>
+                             <input  type="url" id="website" name="website" onChange={(e)=>handleInput(e.target.name, e.target.value)} className="small-input" placeholder="Website"     />
+                        </div>
+                        <div>
+                             <input  type="text" id="headline" name="headline" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' placeholder="Headline"     />
+                        </div>
+                        <div>
+                             <input  type="text" id="summary" name="summary" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' placeholder="Summary"     />
+                        </div>
 
-                        <Grid item lg={12} md={12} sm={12} xs={12} >
+                        <div>
                             <h1>Location</h1>
                            
 
-                        </Grid>
-                        <Grid item  lg={12} md={12} sm={12} xs={12} >
-                             <TextField  type="text" id="address" name="address" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' label="Address" variant="outlined"    />
-                        </Grid>
-                        <Grid item  lg={12} md={12} sm={12} xs={12} >
-                             <TextField  type="url" id="linkedIn" name="linkedin" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' label="Address-Url" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12} sm={12} xs={12} >
-                             <TextField  type="text" id="city" name="city" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' label="City" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12} sm={12} xs={12} >
-                             <TextField  type="text" id="reagion" name="region" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' label="Reagion" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12} sm={12} xs={12} >
-                             <TextField  type="text" id="country" name="country" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' label="Country" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12} sm={12} xs={12} >
-                         <TextField  type="number" id="postalCode" name="postalcode" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input'  label="Postal Code" variant="outlined"    />
-                        </Grid>
-                    </Grid>
+                        </div>
+                        <div>
+                             <input  type="text" id="address" name="address" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' placeholder="Address"     />
+                        </div>
+                        <div>
+                             <input  type="url" id="linkedIn" name="linkedin" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' placeholder="Address-Url"     />
+                        </div>
+                        <div>
+                             <input  type="text" id="city" name="city" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' placeholder="City"     />
+                        </div>
+                        <div>
+                             <input  type="text" id="reagion" name="region" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' placeholder="Reagion"     />
+                        </div>
+                        <div>
+                             <input  type="text" id="country" name="country" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' placeholder="Country"     />
+                        </div>
+                        <div>
+                         <input  type="number" id="postalCode" name="postalcode" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input'  placeholder="Postal Code"     />
+                        </div>
+                    </div>
                 </div>    
 
                 <div className="form2">
-                    <Grid container spacing={2} justify-content='center' alignItems='center' >
-                        <Grid item lg={12} md={12}>
+                    <div>
+                        <div>
                             <h1>Education</h1>
                             {/* <Button onClick={handleEducation}><Icon color="primary">add_circle</Icon></Button> */}
-                        </Grid>
-                        <Grid item lg={12} md={12}>
-                             <TextField  type="text" id="college-Name" name="collegename" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' label="School/College Name" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12}>
-                             <TextField  type="text" id="stream" name="stream" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' label="Stream" variant="outlined"    />
-                        </Grid >
-                        <Grid item lg={6} md={12}>
-                         <TextField  type="text" id="grade" name="grade" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input'  label="Grade" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12}>
-                             <TextField   id="startDate" name="startdate" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' label="Start Date" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12}>
-                             <TextField  id="endDate" name="enddate" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' label="End Date" variant="outlined"    />
-                        </Grid>
-                    </Grid>
+                        </div>
+                        <div>
+                             <input  type="text" id="college-Name" name="collegename" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' placeholder="School/College Name"     />
+                        </div>
+                        <div>
+                             <input  type="text" id="stream" name="stream" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' placeholder="Stream"     />
+                        </div >
+                        <div>
+                         <input  type="text" id="grade" name="grade" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input'  placeholder="Grade"     />
+                        </div>
+                        <div>
+                             <input   id="startDate" name="startdate" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' placeholder="Start Date"     />
+                        </div>
+                        <div>
+                             <input  id="endDate" name="enddate" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' placeholder="End Date"     />
+                        </div>
+                    </div>
                 </div> 
 
                 <div  id="form3">
-                    <Grid container spacing={2} justify-content='center' alignItems='center' >
-                        <Grid item lg={12} md={12}>
+                    <div>
+                        <div>
                             <h1>Work Experience</h1>
                             {/* <Button onClick={handleExperience}><Icon color="primary">add_circle</Icon></Button> */}
-                        </Grid>
-                        <Grid item lg={12} md={12}>
-                             <TextField  type="text" id="company-Name" name="companyname" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' label="Company Name" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12}>
-                             <TextField  type="text" id="designation" name="designation" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' label="Designation" variant="outlined"    />
-                        </Grid >
-                        <Grid item lg={6} md={12}>
-                         <TextField  type="url" id="projectlink" name="projectlink" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input'  label="ProjctLink" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12}>
-                             <TextField   id="startDate"  name="jobstartdate" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' label="Start Date" variant="outlined"    />
-                        </Grid>
-                        <Grid item lg={6} md={12}>
-                             <TextField   id="endDate" name="jobenddate" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' label="End Date" variant="outlined"    />
-                        </Grid>
-                        <Grid item>
-                             <TextField  type="text" id="description" name="description" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' label="Description" variant="outlined"   />
-                        </Grid>
+                        </div>
+                        <div>
+                             <input  type="text" id="company-Name" name="companyname" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' placeholder="Company Name"     />
+                        </div>
+                        <div>
+                             <input  type="text" id="designation" name="designation" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' placeholder="Designation"     />
+                        </div >
+                        <div>
+                         <input  type="url" id="projectlink" name="projectlink" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input'  placeholder="ProjctLink"     />
+                        </div>
+                        <div>
+                             <input   id="startDate"  name="jobstartdate" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' placeholder="Start Date"     />
+                        </div>
+                        <div>
+                             <input   id="endDate" name="jobenddate" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='small-input' placeholder="End Date"     />
+                        </div>
+                        <div>
+                             <input  type="text" id="description" name="description" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input' placeholder="Description"    />
+                        </div>
                         
-                    </Grid>
+                    </div>
                 </div>
 
                 <div  id="form4" >
-                    <Grid container spacing={2} justify-content='center' alignItems='center' >
-                        <Grid item lg={12} md={12}>
+                    <div>
+                        <div>
                                 <h1>Skills</h1>
-                        </Grid>
-                        <Grid item lg={12} md={12}>
-                             <TextField  type="text" id="skill" name="skills" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input'  label="Add Skills" variant="outlined"    />
-                        </Grid>
-                    </Grid>
+                        </div>
+                        <div>
+                             <input  type="text" id="skill" name="skills" onChange={(e)=>handleInput(e.target.name, e.target.value)} className='input'  placeholder="Add Skills"     />
+                        </div>
+                    </div>
                 </div>
                 
-                <Grid container spacing={2} justify-content='center' alignItems='center' >
-                    <Grid id="btn" item lg={12} md={12} sm={12} xs={12}>
+                <div>
+                    <div>
                         <Button id="button" type='submit' variant="contained" ><span></span>{buttondata?"Preview":"Download"}</Button>
-                    </Grid>
-                </Grid>
+                    </div>
+                </div>
                 
 
             </form>
